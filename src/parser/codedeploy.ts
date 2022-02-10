b4bfbedd4559de90855cec97dd828f7c69984197
Benjamin Yu <ben@previewme.co>
2022-02-10 15:29:19 +1300

    Change to use attatchment. Add colour

--- src/parser/codedeploy.ts
+++ src/parser/codedeploy.ts
@@ -1,4 +1,4 @@
-import { buildFieldSection, buildHeader, buildTextSection, SlackMessage } from '../slack';
+import { buildFieldSection, buildHeader, buildTextSection, COLOURS, SlackMessage } from '../slack';
 
 export interface CodedeployEvent {
     region: string;
@@ -28,18 +28,32 @@ export async function parse(event: CodedeployEvent, subject: string): Promise<Sl
 function buildSlackMessage(event: CodedeployEvent, subject: string): SlackMessage {
     const { Failed, InProgress, Pending, Skipped, Succeeded } = event.deploymentOverview;
     const message: SlackMessage = {
-        blocks: [
-            buildHeader(subject),
-            buildFieldSection('mrkdwn', [`*Region*\n${event.region}`, `*Account Id*\n${event.accountId}`]),
-            buildFieldSection('mrkdwn', [`*Event Trigger Name*\n${event.eventTriggerName}`, `*Application Name*\n${event.applicationName}`]),
-            buildFieldSection('mrkdwn', [`*Deployment Id*\n${event.deploymentId}`, `*Deployment Group Name*\n${event.deploymentGroupName}`]),
-            buildFieldSection('mrkdwn', [`*Create Time*\n${event.createTime}`, `*Complete Time*\n${event.completeTime}`]),
-            buildTextSection(
-                'mrkdwn',
-                `*Deployment Overview*\nFailed: ${Failed}, InProgress: ${InProgress}, Pending: ${Pending}, Skipped: ${Skipped}, Succeeded: ${Succeeded}`
-            ),
-            buildTextSection('mrkdwn', `*Status*\n${event.status}`)
+        blocks: [buildHeader(subject), buildTextSection('mrkdwn', `<!channel>`)],
+        attachments: [
+            {
+                color: getMessageColour(event.status),
+                blocks: [
+                    buildFieldSection('mrkdwn', [`*Region*\n${event.region}`, `*Account Id*\n${event.accountId}`]),
+                    buildFieldSection('mrkdwn', [`*Event Trigger Name*\n${event.eventTriggerName}`, `*Application Name*\n${event.applicationName}`]),
+                    buildFieldSection('mrkdwn', [`*Deployment Id*\n${event.deploymentId}`, `*Deployment Group Name*\n${event.deploymentGroupName}`]),
+                    buildFieldSection('mrkdwn', [`*Create Time*\n${event.createTime}`, `*Complete Time*\n${event.completeTime}`]),
+                    buildTextSection(
+                        'mrkdwn',
+                        `*Deployment Overview*\nFailed: ${Failed}, InProgress: ${InProgress}, Pending: ${Pending}, Skipped: ${Skipped}, Succeeded: ${Succeeded}`
+                    ),
+                    buildTextSection('mrkdwn', `*Status*\n${event.status}`)
+                ]
+            }
         ]
     };
     return message;
+}
+
+function getMessageColour(status: string): string {
+    switch (status) {
+        case 'Failed':
+            return COLOURS.critical;
+        default:
+            return COLOURS.neutral;
+    }
 }
