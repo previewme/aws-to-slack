815b9534ccfb507e70fc4bef75610a71d219687d
Benjamin Yu <ben@previewme.co>
2022-02-10 14:40:35 +1300

    Pass test to match code deploy slack message

--- __tests__/parser/codedeploy.test.ts
+++ __tests__/parser/codedeploy.test.ts
@@ -1,5 +1,7 @@
 import { default as codedeployEvent } from '../resources/codedeploy-event.json';
+import { default as codedeploySlack } from '../resources/codedeploy-slack.json';
 import { match } from '../../src/parser/matcher';
+import { parse } from '../../src/parser/codedeploy';
 
 describe('Ensure Codedeploy events can be processed', () => {
     test('Codedeploy alarm message matches', async () => {
@@ -9,5 +11,10 @@ describe('Ensure Codedeploy events can be processed', () => {
 
     test('Not a Codedeploy event', async () => {
         await expect(match('{}')).rejects.toThrowError('Not a codedeploy event');
+    });
+
+    test('Parse event, should match codedeploySlack format', async () => {
+        const actual = await parse(codedeployEvent, 'CREATED: AWS CodeDeploy d-CLYPG9180 in ap-south-1 to MyApp-sample-app');
+        expect(actual).toEqual(codedeploySlack);
     });
 });
--- src/parser/codedeploy.ts
+++ src/parser/codedeploy.ts
@@ -1,3 +1,5 @@
+import { buildFieldSection, buildHeader, buildTextSection, SlackMessage } from '../slack';
+
 export interface CodedeployEvent {
     region: string;
     accountId: string;
@@ -7,5 +9,37 @@ export interface CodedeployEvent {
     deploymentGroupName: string;
     createTime: string;
     completeTime: string;
+    deploymentOverview: DeploymentOverview;
     status: string;
+}
+
+interface DeploymentOverview {
+    Failed: string;
+    InProgress: string;
+    Pending: string;
+    Skipped: string;
+    Succeeded: string;
+}
+
+export async function parse(event: CodedeployEvent, subject: string): Promise<SlackMessage> {
+    return buildSlackMessage(event, subject);
+}
+
+function buildSlackMessage(event: CodedeployEvent, subject: string): SlackMessage {
+    const { Failed, InProgress, Pending, Skipped, Succeeded } = event.deploymentOverview;
+    const message: SlackMessage = {
+        blocks: [
+            buildHeader(subject),
+            buildFieldSection('mrkdwn', [`*Region*\n${event.region}`, `*Account Id*\n${event.accountId}`]),
+            buildFieldSection('mrkdwn', [`*Event Trigger Name*\n${event.eventTriggerName}`, `*Application Name*\n${event.applicationName}`]),
+            buildFieldSection('mrkdwn', [`*Deployment Id*\n${event.deploymentId}`, `*Deployment Group Name*\n${event.deploymentGroupName}`]),
+            buildFieldSection('mrkdwn', [`*Create Time*\n${event.createTime}`, `*Complete Time*\n${event.completeTime}`]),
+            buildTextSection(
+                'mrkdwn',
+                `*Deployment Overview*\nFailed: ${Failed}, InProgress: ${InProgress}, Pending: ${Pending}, Skipped: ${Skipped}, Succeeded: ${Succeeded}`
+            ),
+            buildTextSection('mrkdwn', `*Status*\n${event.status}`)
+        ]
+    };
+    return message;
 }
